87e92f3c7fd2d7cec2d5689bef8d8e37ccae5f66
sahirooh <sahirooh@gmail.com>
2023-12-17 19:47:14 +0530

    updated layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
   return (
     <main>
+      <h1>BookMyEvent</h1>
       <Button variant='destructive'>
         Page
       </Button>
