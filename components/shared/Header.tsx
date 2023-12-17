d879bc84bb746f86340882712b83121a020c6ff0
sahirooh <sahirooh@gmail.com>
2023-12-17 23:42:05 +0530

    updated header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="w-full border-b ">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image src="/assets/images/logo.svg" alt="logo" width={128} height={36} />
+        </Link>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
