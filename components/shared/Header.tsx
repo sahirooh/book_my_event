6ae59233b50a90d5965d878a722feaf6da479d5e
sahirooh <sahirooh@gmail.com>
2023-12-18 19:14:20 +0530

    updated header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -7,7 +7,12 @@ const Header = () => {
     <header className="w-full border-b ">
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
-          <Image src="/assets/images/logo.svg" alt="logo" width={128} height={36} />
+          {/* <Image src="/assets/images/logo.svg" alt="logo" width={128} height={36} /> */}
+          <h1>
+            <span className="text-red-500">B</span>ook
+            <span className="text-red-500">M</span>y
+            <span className="text-red-500">E</span>vent
+          </h1>
         </Link>
       </div>
     </header>
