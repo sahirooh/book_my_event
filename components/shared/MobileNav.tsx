d4f2da096af5cc8997096e98f06cd7c2de5ffb1e
sahirooh <sahirooh@gmail.com>
2023-12-19 23:24:31 +0530

    updated mobileNav

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -7,6 +7,8 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import Image from "next/image";
+import { Separator } from "../ui/separator";
+import NavItems from "./NavItems";
 
 const MobileNav = () => {
   return (
@@ -16,7 +18,9 @@ const MobileNav = () => {
             <Image className="cursor-pointer" src="/assets/icons/menu.svg" alt="menu" width={24} height={24} />
         </SheetTrigger>
         <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
-          <Image className="cursor-pointer" src="/assets/images/logo.svg" alt="logo" width={128} height={38} />
+          <Image src="/assets/images/logo.svg" alt="logo" width={128} height={38} />
+          <Separator className="border border-gray-50" />
+          <NavItems />
         </SheetContent>
       </Sheet>
     </nav>
