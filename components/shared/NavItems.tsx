0869f336c06e0c1f3752e2310210cbfab90e7838
sahirooh <sahirooh@gmail.com>
2023-12-19 23:32:44 +0530

    updated navitems

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,9 +1,21 @@
+import { headerLinks } from '@/constants'
+import Link from 'next/link'
 import React from 'react'
 
 const NavItems = () => {
   return (
-    <ul className='md:flex-between flex'>
-        
+    <ul className='md:flex-between w-full flex-col items-start gap-5 md:flex-row flex'>
+        {
+            headerLinks.map((link) => {
+                return (
+                    <li>
+                        <Link href={link.route}>
+                        {link.label}
+                        </Link>
+                    </li>
+                )
+            })
+        }
     </ul>
   )
 }
