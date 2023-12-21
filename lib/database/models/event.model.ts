df25e2ad37c46903268c0ed54431a16606d9921f
sahirooh <sahirooh@gmail.com>
2023-12-21 21:52:34 +0530

    added event model

---
+++ lib/database/models/event.model.ts
@@ -0,0 +1,5 @@
+import { Schema } from "mongoose";
+
+const EventSchema = new Schema({
+    
+})
--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -10,3 +10,5 @@ const UserSchema = new Schema({
 })
 
 const User = models.User || model('User', UserSchema);
+
+export default User;
