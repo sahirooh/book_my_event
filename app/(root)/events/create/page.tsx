b9e00297bfac76abacc851cd974c19fdd8c28386
sahirooh <sahirooh@gmail.com>
2023-12-23 22:12:32 +0530

    added create event page

---
+++ app/(root)/events/create/page.tsx
@@ -0,0 +1,10 @@
+
+const CreateEvent = () => {
+  return (
+    <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">Create Event</h3>
+    </section>
+  )
+}
+
+export default CreateEvent
