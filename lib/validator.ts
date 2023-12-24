d9f2b21b71f250fc087d79eb10db71bde354eea4
sahirooh <sahirooh@gmail.com>
2023-12-24 22:11:13 +0530

    updated validator

--- lib/validator.ts
+++ lib/validator.ts
@@ -1,5 +1,20 @@
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
+import * as z from "zod";
+
+export const eventFormSchema = z.object({
+  title: z.string().min(3, "Title must be at least 3 characters."),
+  description: z
+    .string()
+    .min(3, "Description must be at least 3 characters.")
+    .max(400, "Description must be less than 400 characters."),
+  location: z
+    .string()
+    .min(3, "Location must be at least 3 characters.")
+    .max(400, "Location must be less than 400 characters."),
+  imageUrl: z.string(),
+  startDateTime: z.date(),
+  endDateTime: z.date(),
+  categoryId: z.string(),
+  price: z.string(),
+  isFree: z.boolean(),
+  url: z.string().url(),
 });
