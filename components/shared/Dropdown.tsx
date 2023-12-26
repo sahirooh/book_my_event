dfee7797857ce4e8587241542c73db455d75c09e
sahirooh <sahirooh@gmail.com>
2023-12-26 21:46:13 +0530

    updated dropdown menu

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -26,9 +26,7 @@ type DropdownProps = {
 };
 
 const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
-  const [categories, setCategories] = useState<ICategory[]>([{
-    _id: "1", name: "Category 1",
-  }]);
+  const [categories, setCategories] = useState<ICategory[]>([]);
 
   return (
     <Select onValueChange={onChangeHandler} defaultValue={value}>
@@ -38,10 +36,30 @@ const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
       <SelectContent>
         {categories.length > 0 &&
           categories.map((category) => (
-            <SelectItem className="select-item p-regular-14" key={category._id} value={category._id}>
+            <SelectItem
+              className="select-item p-regular-14"
+              key={category._id}
+              value={category._id}
+            >
               {category.name}
             </SelectItem>
           ))}
+        <AlertDialog>
+          <AlertDialogTrigger>Open</AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </SelectContent>
     </Select>
   );
