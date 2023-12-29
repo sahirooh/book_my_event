3bddbaa75db5bc6412afc9455eeeb5d8ba18d8af
sahirooh <sahirooh@gmail.com>
2023-12-29 19:40:21 +0530

    updated event form

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -270,9 +270,40 @@ const EventForm = ({ userId, type }: EventFormProps) => {
               </FormItem>
             )}
           />
+          <FormField
+            control={form.control}
+            name="url"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-gray-50 px-4 py-2">
+                    <Image
+                      src="/assets/icons/link.svg"
+                      alt="link"
+                      width={24}
+                      height={24}
+                    />
+                    <Input
+                      placeholder="URL"
+                      {...field}
+                      className="input-field"
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
 
-        <Button type="submit">Submit</Button>
+        <Button
+          size="lg"
+          disabled={form.formState.isSubmitting}
+          className="button col-span-2 w-full"
+          type="submit"
+        >
+          {form.formState.isSubmitting ? "Submitting..." : `${type} Event}`}
+        </Button>
       </form>
     </Form>
   );
