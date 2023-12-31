9a7af58180f348d93d15cec03733f17e5b0afa57
sahirooh <sahirooh@gmail.com>
2023-12-31 19:03:15 +0530

    updated event actions

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -4,12 +4,19 @@ import { CreateEventParams } from "@/types"
 import { connectToDatabase } from "../database"
 import { handleError } from "../utils"
 import User from "../database/models/user.model"
+import Event from "../database/models/event.model"
 
 export const createEvent = async ({event, userId, path}: CreateEventParams) => {
     try {
         await connectToDatabase()
 
         const organizer = await User.findById(userId);
+
+        if(!organizer) {
+            throw new Error("User not found");
+        }
+
+        const newEvent = await Event.create({...event, category: event.categoryId, organizer: userId});
     } catch (error) {
         handleError(error);
     }
