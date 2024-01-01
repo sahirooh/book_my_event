fc35a507619ab8441614ed575297122bbe206f0c
sahirooh <sahirooh@gmail.com>
2024-01-01 19:45:42 +0530

    updated event actions

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -24,3 +24,17 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
     handleError(error);
   }
 }
+
+export const getEventById = async (eventId: string) => {
+  try {
+    await connectToDatabase();
+
+    const event = await Event.findById(eventId);
+
+    if(!event) throw new Error("Event not found");
+
+    return JSON.parse(JSON.stringify(event));
+  } catch (error) {
+    handleError(error);
+  }
+}
