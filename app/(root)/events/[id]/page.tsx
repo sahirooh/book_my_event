02eebc1fe6f369474c56a571cc6c1443a9a3059a
sahirooh <sahirooh@gmail.com>
2024-01-01 19:43:13 +0530

    init event details page

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const page = () => {
+const EventDetails = () => {
   return (
-    <div>page</div>
+    <div>EventDetails</div>
   )
 }
 
-export default page
+export default EventDetails
