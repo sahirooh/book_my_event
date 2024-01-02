2feea3a3ab16b7e316bdfdaf511a26fe46523d6a
sahirooh <sahirooh@gmail.com>
2024-01-02 21:24:23 +0530

    updated eventDetails page

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -46,12 +46,11 @@ const EventDetails = async ({ params: { id } }: SearchParamProps) => {
               <Image src='/assets/icons/calendar.svg' alt="calendar" width={32} height={32} />
               <div className="p-medium-16 lg:p-regular-20 flex flex-wrap items-center">
                 <p>
-                  {formatDateTime(event.startDateTime).dateOnly}
+                  {formatDateTime(event.startDateTime).dateOnly} - {' '}
+                  {formatDateTime(event.startDateTime).timeOnly}
                 </p>
-                <p className="ml-1">
-                  {formatDateTime(event.startDateTime).timeOnly} - {' '}
-                </p>
-                <p className="ml-1">
+                <p>
+                  {formatDateTime(event.endDateTime).dateOnly} - {' '}
                   {formatDateTime(event.endDateTime).timeOnly}
                 </p>
               </div>
