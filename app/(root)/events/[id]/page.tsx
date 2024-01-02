2740901c69674db0f0be1756548d7340ead9f6fd
sahirooh <sahirooh@gmail.com>
2024-01-02 18:59:36 +0530

    updated eventdetails page

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -20,6 +20,15 @@ const EventDetails = async ({ params: { id } }: SearchParamProps) => {
         <div className="flex w-full flex-col gap-8 p-5 md:p-10">
           <div className="flex flex-col gap-6">
             <h2 className="h2-bold">{event.title}</h2>
+
+            <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
+              <div className="flex gap-3">
+                <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
+                  {event.isFree ? "FREE" : `$${event.price}`}
+                </p>
+                <p></p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
