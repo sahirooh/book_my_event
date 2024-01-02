4627a9dda352329d9c5c1666e8c096f8c158697a
sahirooh <sahirooh@gmail.com>
2024-01-02 19:29:20 +0530

    updated eventDetails page

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -30,6 +30,13 @@ const EventDetails = async ({ params: { id } }: SearchParamProps) => {
                   {event.category.name}
                 </p>
               </div>
+
+              <p className="p-medium-18 ml-2 mt-2 sm:mt-0">
+                by{" "}
+                <span className="text-primary-500">
+                  {event.organizer.firstName} {event.organizer.lastName}
+                </span>
+              </p>
             </div>
           </div>
         </div>
