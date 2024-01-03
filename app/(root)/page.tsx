82a3999c32e704bc5ee01611ad95c50a09d67743
sahirooh <sahirooh@gmail.com>
2024-01-03 19:59:55 +0530

    updated HomePage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,18 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/lib/actions/event.actions";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+
+  const events = await getAllEvents({
+    query: '',
+    category: '',
+    page: 1,
+    limit: 6,
+  });
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
