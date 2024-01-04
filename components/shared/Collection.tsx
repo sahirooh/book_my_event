b0a0cb5084e6c30dad5d1002ec50c580362742f1
sahirooh <sahirooh@gmail.com>
2024-01-04 19:25:11 +0530

    updated collections

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -25,11 +25,13 @@ const Collection = ({
   return (
     <>
       {data.length > 0 ? (
-        <div></div>
-      ) : (
         <div>
-          <h3>{emptyTitle}</h3>
-          <p>{emptyStateSubtext}</p>
+
+        </div>
+      ) : (
+        <div className="flex-center wrapper min-h-[200px] w-full flex-col gap-3 rounded-[14px] bg-grey-50 py-28 text-center">
+          <h3 className="p-bold-20 md:h5-bold">{emptyTitle}</h3>
+          <p className="p-regular-14">{emptyStateSubtext}</p>
         </div>
       )}
     </>
