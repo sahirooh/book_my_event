160f40c44e0e3dcf296a5a54b666bc2f3592063d
sahirooh <sahirooh@gmail.com>
2024-01-04 22:09:57 +0530

    updated card

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { IEvent } from '@/lib/database/models/event.model'
 
-const Card = () => {
+type CardProps = {
+    event: IEvent,
+    hasOrderLink?: boolean,
+    hidePrice?: boolean
+}
+
+const Card = ({event, hasOrderLink, hidePrice}: CardProps) => {
   return (
-    <div>Card</div>
+    <div className='group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[440px]'>
+        
+    </div>
   )
 }
 
