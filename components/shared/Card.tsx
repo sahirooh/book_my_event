9a621ef60a29aa44c2f91b4c5cac9f5938f8e0d3
sahirooh <sahirooh@gmail.com>
2024-01-05 20:01:31 +0530

    updated card

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,4 +1,5 @@
 import { IEvent } from "@/lib/database/models/event.model";
+import { formatDateTime } from "@/lib/utils";
 import Link from "next/link";
 
 type CardProps = {
@@ -20,11 +21,24 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
         href={`/events/${event._id}`}
         className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4"
       >
-        <div className="flex gap-2">
-          <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
-            {event.isFree ? "FREE" : `$${event.price}`}
-          </span>
-        </div>
+        {!hidePrice && (
+          <div className="flex gap-2">
+            <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
+              {event.isFree ? "FREE" : `$${event.price}`}
+            </span>
+            <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500">
+              {event.category.name}
+            </p>
+          </div>
+        )}
+
+        <p className="p-medium-16 p-medium-18 text-grey-500">
+          {formatDateTime(event.startDateTime).dateTime}
+        </p>
+
+        <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">
+          {event.title}
+        </p>
       </Link>
     </div>
   );
