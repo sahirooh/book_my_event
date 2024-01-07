d0680c97523e398ea34ee4742708b506a8f100e2
sahirooh <sahirooh@gmail.com>
2024-01-07 21:46:20 +0530

    added related events section

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,12 +1,18 @@
 import Collection from "@/components/shared/Collection";
-import { getEventById } from "@/lib/actions/event.actions";
+import { getEventById, getRelatedEventsByCategory } from "@/lib/actions/event.actions";
 import { formatDateTime } from "@/lib/utils";
 import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import React from "react";
 
-const EventDetails = async ({ params: { id } }: SearchParamProps) => {
+const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) => {
   const event = await getEventById(id);
+
+  const relatedEvents = await getRelatedEventsByCategory({
+    categoryId: event.category._id,
+    eventId: event._id,
+    page: searchParams.page as string,
+  })
 
   return (
     <>
@@ -88,7 +94,7 @@ const EventDetails = async ({ params: { id } }: SearchParamProps) => {
       <section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
         <h2 className="h2-bold">Related Events</h2>
         <Collection
-          data={events?.data}
+          data={relatedEvents?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
           collectionType="All_Events"
