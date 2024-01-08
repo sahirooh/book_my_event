0a1ef3abd87372fe6cbbac2c0f3c1d667ea6b372
sahirooh <sahirooh@gmail.com>
2024-01-08 20:06:22 +0530

    added create event

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -27,6 +27,15 @@ const ProfilePage = () => {
           totalPages={2}
         />
       </section>
+
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
+          <h3 className="h3-bold text-center sm:text-left">Events Organized</h3>
+          <Button asChild className="button hidden sm:flex">
+            <Link href="/events/create">Create New Event</Link>
+          </Button>
+        </div>
+      </section>
     </>
   );
 };
