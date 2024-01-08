9ab923dc74bf191db171160799e08de3e05657d8
sahirooh <sahirooh@gmail.com>
2024-01-08 19:59:29 +0530

    init MyProfile page

---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProfilePage = () => {
+  return (
+    <>
+    <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
+
+    </section>
+    </>
+  )
+}
+
+export default ProfilePage
