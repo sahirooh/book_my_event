1dc692a68376829c305744769509c509b7db0b6a
sahirooh <sahirooh@gmail.com>
2024-01-08 20:02:23 +0530

    added button

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const ProfilePage = () => {
@@ -6,6 +8,11 @@ const ProfilePage = () => {
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
+          <Button asChild className="button hidden sm:flex">
+            <Link href='/#events'>
+            Explore More Events
+            </Link>
+          </Button>
         </div>
       </section>
     </>
