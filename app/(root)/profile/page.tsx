6849c79827c1d50238ca75af1ab531ea79a218b1
sahirooh <sahirooh@gmail.com>
2024-01-08 20:04:45 +0530

    added collections

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,3 +1,4 @@
+import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
@@ -9,11 +10,22 @@ const ProfilePage = () => {
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
           <Button asChild className="button hidden sm:flex">
-            <Link href='/#events'>
-            Explore More Events
-            </Link>
+            <Link href="/#events">Explore More Events</Link>
           </Button>
         </div>
+      </section>
+
+      <section className="wrapper my-8">
+        <Collection
+          data={events?.data}
+          emptyTitle="No event ticktes purchased yet"
+          emptyStateSubtext="No worries - plenty of events to choose from!"
+          collectionType="My_Tickets"
+          limit={3}
+          page={1}
+          urlParamName="ordersPage"
+          totalPages={2}
+        />
       </section>
     </>
   );
