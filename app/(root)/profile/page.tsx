0cfed6e9f4640ab55a4a4af8c627fac64e0a3ed0
sahirooh <sahirooh@gmail.com>
2024-01-08 20:00:44 +0530

    updated myProfile page

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const ProfilePage = () => {
   return (
     <>
-    <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
-
-    </section>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
+          <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
