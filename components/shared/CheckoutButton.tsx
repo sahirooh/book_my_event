9303b32216fce0ab1960741b81912d7433319885
sahirooh <sahirooh@gmail.com>
2024-01-10 19:46:00 +0530

    added signout clerk

--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -1,15 +1,33 @@
 "use client";
 
 import { IEvent } from "@/lib/database/models/event.model";
-import { useUser } from "@clerk/nextjs";
+import { SignedOut, useUser } from "@clerk/nextjs";
 import React from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 const CheckoutButton = ({ event }: { event: IEvent }) => {
   const { user } = useUser();
   const userId = user?.publicMetadata.userId as string;
   const hasEventFinished = new Date(event.endDateTime) < new Date();
 
-  return <div className="flex items-center gap-3">CheckoutButton</div>;
+  return (
+    <div className="flex items-center gap-3">
+      {hasEventFinished ? (
+        <p className="p-2 text-red-400">Tickets are no longer available</p>
+      ) : (
+        <>
+          <SignedOut>
+            <Button>
+              <Link href='/sign-in'>
+                Get Tickets
+              </Link>
+            </Button>
+          </SignedOut>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default CheckoutButton;
