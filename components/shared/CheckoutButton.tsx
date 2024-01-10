efe2954a6a5713c238d2b8eb4da50ce817124eeb
sahirooh <sahirooh@gmail.com>
2024-01-10 19:53:24 +0530

    updated SignedIn

--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -5,6 +5,7 @@ import { SignedIn, SignedOut, useUser } from "@clerk/nextjs";
 import React from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import Checkout from "./Checkout";
 
 const CheckoutButton = ({ event }: { event: IEvent }) => {
   const { user } = useUser();
@@ -26,7 +27,7 @@ const CheckoutButton = ({ event }: { event: IEvent }) => {
           </SignedOut>
 
           <SignedIn>
-            
+            <Checkout event={event} userId={userId} />
           </SignedIn>
         </>
       )}
