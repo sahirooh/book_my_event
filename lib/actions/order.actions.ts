781d052d9f05472f573ba78c86185bcf18df5c47
sahirooh <sahirooh@gmail.com>
2024-01-11 21:40:04 +0530

    added order actions

---
+++ lib/actions/order.actions.ts
@@ -0,0 +1,11 @@
+'use server'
+
+import { CheckoutOrderParams } from "@/types"
+
+export const checkoutOrder = async (order: CheckoutOrderParams) => {
+    try {
+        
+    } catch (error) {
+        throw error;
+    }
+}
