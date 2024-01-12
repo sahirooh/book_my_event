ed704d33b6fdf76022ddde2d1a79a6cf86bd61ae
sahirooh <sahirooh@gmail.com>
2024-01-12 19:49:43 +0530

    updated stripe route

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -2,4 +2,13 @@ const eventType = event?.type;
 
 if(eventType === 'checkout.session.completed') {
     const {id, amount_total, metadata} = event.data.object;
+
+    const order = {
+        stripeId: id,
+        eventId: metadata?.eventId || '',
+        buyerId: metadata?.buyerId || '',
+        totalAmout: amount_total ? (amount_total / 100).toString() : '0',
+        createdAt: new Date(),
+    }
+    
 }
