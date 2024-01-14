fa1e7d9673c170e8ba3343fc7143ce6ae6f1ea3d
sahirooh <sahirooh@gmail.com>
2024-01-14 20:02:14 +0530

    updated ProfilePage

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -3,13 +3,17 @@ import { Button } from "@/components/ui/button";
 import { getEventsByUser } from "@/lib/actions/event.actions";
 import { getOrdersByUser } from "@/lib/actions/order.actions";
 import { IOrder } from "@/lib/database/models/order.model";
+import { SearchParamProps } from "@/types";
 import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
 
-const ProfilePage = async () => {
+const ProfilePage = async ({searchParams}: SearchParamProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
+
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+  
 
   const orders = await getOrdersByUser({userId, page: 1})
 
