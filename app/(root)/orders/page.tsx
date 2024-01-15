ab1ec759fa1594f84608bca02ea6be2f58712397
sahirooh <sahirooh@gmail.com>
2024-01-15 22:11:52 +0530

    updated ordersPage

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -2,6 +2,11 @@ import { SearchParamProps } from '@/types'
 import React from 'react'
 
 const OrdersPage = async ({searchParams}: SearchParamProps) => {
+  const eventId = (searchParams?.eventId as string) || ''
+  const searchText = (searchParams?.query as string) || ''
+
+  
+  
   return (
     <div>OrdersPage</div>
   )
