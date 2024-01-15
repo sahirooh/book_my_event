4e7ecd34770bacccb3034cefe34426b3ece8b295
sahirooh <sahirooh@gmail.com>
2024-01-15 22:17:27 +0530

    added search

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -1,3 +1,4 @@
+import Search from '@/components/shared/Search'
 import { getOrdersByEvent } from '@/lib/actions/order.actions'
 import { SearchParamProps } from '@/types'
 import React from 'react'
@@ -12,6 +13,10 @@ const OrdersPage = async ({searchParams}: SearchParamProps) => {
     <>
     <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
       <h3 className='wrapper h3-bold text-center sm:text-left'>Orders</h3>
+    </section>
+
+    <section className='wrapper mt-8'>
+      <Search placeholder='Search buyer name' />
     </section>
     </>
   )
