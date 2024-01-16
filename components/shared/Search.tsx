396ab3014035b42683234da1de8b50afb80bad79
sahirooh <sahirooh@gmail.com>
2024-01-16 19:17:21 +0530

    updated search

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -1,13 +1,15 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const Search = ({placeholder} :{placeholder: string}) => {
-  const [query, setQuery] = useState('');
+const Search = ({ placeholder }: { placeholder: string }) => {
+  const [query, setQuery] = useState("");
 
   return (
-    <div>Search</div>
-  )
-}
+    <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+      Search
+    </div>
+  );
+};
 
-export default Search
+export default Search;
