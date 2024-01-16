0e8eab765b03fe18afd1187e91e6926d4fb78d76
sahirooh <sahirooh@gmail.com>
2024-01-16 19:21:48 +0530

    added input

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import React, { useState } from "react";
+import { Input } from "../ui/input";
 
 const Search = ({ placeholder }: { placeholder: string }) => {
   const [query, setQuery] = useState("");
@@ -9,6 +10,7 @@ const Search = ({ placeholder }: { placeholder: string }) => {
   return (
     <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
       <Image src='/assets/icons/search.svg' alt="search" width={24} height={24} />
+      <Input type="text" placeholder="Search" onChange={(e) => setQuery(e.target.value)} className="p-regular-16 border-0 bg-grey-50 outline-offset-0 placeholder:text-grey-500 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0" />
     </div>
   );
 };
