6bc6583f19b06535362fa959e9f638ecdf9cca59
sahirooh <sahirooh@gmail.com>
2024-01-16 19:44:53 +0530

    added Debounce fn

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -1,12 +1,26 @@
 "use client";
 
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Input } from "../ui/input";
+import { formUrlQuery } from "@/lib/utils";
+import { useSearchParams } from "next/navigation";
 
 const Search = ({ placeholder }: { placeholder: string }) => {
   const [query, setQuery] = useState("");
+  const searchParams = useSearchParams();
 
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+    if(query){
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: 'query',
+        value: query
+      })
+    }
+    }, 300)
+  }, [query])
   return (
     <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
       <Image src='/assets/icons/search.svg' alt="search" width={24} height={24} />
