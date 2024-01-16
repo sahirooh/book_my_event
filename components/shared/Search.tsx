d2b07130b069660cfebd9d50399ce840a9a27d74
sahirooh <sahirooh@gmail.com>
2024-01-16 19:18:28 +0530

    added image

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import React, { useState } from "react";
 
 const Search = ({ placeholder }: { placeholder: string }) => {
@@ -7,7 +8,7 @@ const Search = ({ placeholder }: { placeholder: string }) => {
 
   return (
     <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
-      Search
+      <Image src='/assets/icons/search.svg' alt="search" width={24} height={24} />
     </div>
   );
 };
