bfb6e0b579fe514e35b69a84fcb537bde11a413b
sahirooh <sahirooh@gmail.com>
2024-01-17 22:49:38 +0530

    updated search

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -13,6 +13,7 @@ const Search = ({ placeholder }: { placeholder: string }) => {
 
   useEffect(() => {
     const delayDebounceFn = setTimeout(() => {
+      let newUrl = '';
     if(query){
       const newUrl = formUrlQuery({
         params: searchParams.toString(),
@@ -28,7 +29,9 @@ const Search = ({ placeholder }: { placeholder: string }) => {
 
     router.push(newUrl, {scroll: false})
     }, 300)
-  }, [query])
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [query, searchParams, router])
   return (
     <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
       <Image src='/assets/icons/search.svg' alt="search" width={24} height={24} />
