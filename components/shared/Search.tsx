6f38a5d09e8a2f964ea05b08ad07e2c648d5ab6e
sahirooh <sahirooh@gmail.com>
2024-01-18 19:10:42 +0530

    updated placeholder

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -35,7 +35,7 @@ const Search = ({ placeholder = 'Search title...' }: { placeholder?: string }) =
   return (
     <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
       <Image src='/assets/icons/search.svg' alt="search" width={24} height={24} />
-      <Input type="text" placeholder="Search" onChange={(e) => setQuery(e.target.value)} className="p-regular-16 border-0 bg-grey-50 outline-offset-0 placeholder:text-grey-500 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0" />
+      <Input type="text" placeholder={placeholder} onChange={(e) => setQuery(e.target.value)} className="p-regular-16 border-0 bg-grey-50 outline-offset-0 placeholder:text-grey-500 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0" />
     </div>
   );
 };
