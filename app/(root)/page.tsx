97bad6ddfa32b241e4e30c4d785ad79b80c77e42
sahirooh <sahirooh@gmail.com>
2024-01-19 22:52:32 +0530

    added categoryFilter

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import CategoryFilter from "@/components/shared/CategoryFilter";
 import Collection from "@/components/shared/Collection";
 import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
@@ -53,7 +54,7 @@ const category = (searchParams?.category as string) || '';
         </h2>
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
-          
+          <CategoryFilter />
         </div>
         <Collection
           data={events?.data}
