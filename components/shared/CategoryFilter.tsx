e86bad2390abc3170d7e26a830e1756494ae0c69
sahirooh <sahirooh@gmail.com>
2024-01-19 22:58:20 +0530

    added shadcn select

--- components/shared/CategoryFilter.tsx
+++ components/shared/CategoryFilter.tsx
@@ -8,12 +8,17 @@ import {
 
 const CategoryFilter = () => {
   return (
-  <Select>
-    <SelectTrigger>
-        <SelectValue></SelectValue>
-    </SelectTrigger>
-  </Select>
-    );
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Theme" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="light">Light</SelectItem>
+        <SelectItem value="dark">Dark</SelectItem>
+        <SelectItem value="system">System</SelectItem>
+      </SelectContent>
+    </Select>
+  );
 };
 
 export default CategoryFilter;
