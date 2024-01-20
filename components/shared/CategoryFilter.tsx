ab55d617984010eb0e72d46696db3f293505d369
sahirooh <sahirooh@gmail.com>
2024-01-20 19:36:03 +0530

    added debounceFn

--- components/shared/CategoryFilter.tsx
+++ components/shared/CategoryFilter.tsx
@@ -7,6 +7,31 @@ import {
 } from "@/components/ui/select";
 
 const CategoryFilter = () => {
+  const [query, setQuery] = useState("");
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      let newUrl = "";
+      if (query) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "query",
+          value: query,
+        });
+      } else {
+        const newUrl = removeKeysFromQuery({
+          params: searchParams.toString(),
+          keysToRemove: ["query"],
+        });
+      }
+
+      router.push(newUrl, { scroll: false });
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [query, searchParams, router]);
   return (
     <Select>
       <SelectTrigger className="w-[180px]">
