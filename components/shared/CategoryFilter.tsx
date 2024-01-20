67b28c30130e36c436dd752e063fe66906e16f25
sahirooh <sahirooh@gmail.com>
2024-01-20 19:37:06 +0530

    updated catFilter

--- components/shared/CategoryFilter.tsx
+++ components/shared/CategoryFilter.tsx
@@ -5,6 +5,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
