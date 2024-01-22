294a3475671b026c8b563ebe1f4405cb0b6aed3c
sahirooh <sahirooh@gmail.com>
2024-01-22 21:40:19 +0530

    removed useEffect

--- components/shared/CategoryFilter.tsx
+++ components/shared/CategoryFilter.tsx
@@ -28,29 +28,9 @@ const CategoryFilter = () => {
     getCategories();
   }, [])
 
-  useEffect(() => {
-    const delayDebounceFn = setTimeout(() => {
-      let newUrl = "";
-      if (categories) {
-        const newUrl = formUrlQuery({
-          params: searchParams.toString(),
-          key: "query",
-          value: categories,
-        });
-      } else {
-        const newUrl = removeKeysFromQuery({
-          params: searchParams.toString(),
-          keysToRemove: ["query"],
-        });
-      }
+  const onSelectCategory = (category: string) => {
 
-      router.push(newUrl, { scroll: false });
-    }, 300);
-
-    return () => clearTimeout(delayDebounceFn);
-  }, [categories, searchParams, router]);
-
-  const onSelectCategory = (category: string) => {};
+  };
 
   return (
     <Select onValueChange={(value: string) => onSelectCategory(value)}>
