a54e71e3e9196a94ce9157a2d0a16aed0f0ac673
sahirooh <sahirooh@gmail.com>
2024-01-23 22:22:39 +0530

    updated props

--- components/shared/Pagination.tsx
+++ components/shared/Pagination.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 type PaginationProps = {
     page: number | string,
+    totalPages: number,
+    urlParamName: string
 }
 
 const Pagination = ({urlParamName, page, totalPages}: PaginationProps) => {
