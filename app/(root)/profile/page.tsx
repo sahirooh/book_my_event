74d29940288dd20b10f95aec0be97da9ca3900de
sahirooh <sahirooh@gmail.com>
2024-01-28 22:09:01 +0530

    updated button

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -8,14 +8,14 @@ import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
 
-const ProfilePage = async ({searchParams}: SearchParamProps) => {
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
   const ordersPage = Number(searchParams?.ordersPage) || 1;
   const eventsPage = Number(searchParams?.eventsPage) || 1;
 
-  const orders = await getOrdersByUser({userId, page: ordersPage})
+  const orders = await getOrdersByUser({ userId, page: ordersPage });
 
   const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
   const organizedEvents = await getEventsByUser({ userId, page: eventsPage });
@@ -25,7 +25,7 @@ const ProfilePage = async ({searchParams}: SearchParamProps) => {
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
-          <Button asChild size='lg' className="button hidden sm:flex">
+          <Button asChild size="lg" className="button hidden sm:flex">
             <Link href="/#events">Explore More Events</Link>
           </Button>
         </div>
@@ -47,7 +47,7 @@ const ProfilePage = async ({searchParams}: SearchParamProps) => {
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">Events Organized</h3>
-          <Button asChild size='lg' className="button hidden sm:flex">
+          <Button asChild size="lg" className="button hidden sm:flex">
             <Link href="/events/create">Create New Event</Link>
           </Button>
         </div>
