79c2ff14a7c5b2ce89930f294373a4e1beb1b1d7
sahirooh <sahirooh@gmail.com>
2024-01-28 22:09:58 +0530

    updated button

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -26,7 +26,6 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
           <Button asChild size="lg" className="button hidden sm:flex">
-            <Link href="/#events">Explore More Events</Link>
           </Button>
         </div>
       </section>
