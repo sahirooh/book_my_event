389e2bdb242d5e42d360e6674cf7e9e2957d21bc
sahirooh <sahirooh@gmail.com>
2024-02-02 20:18:06 +0530

    updated onSubmit fn

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -57,6 +57,7 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
   });
 
   async function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    let uploadedImageUrl = values.imageUrl;
 
     if (files.length > 0) {
       const uploadedImages = await startUpload(files);
