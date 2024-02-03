2c54ddf463556a4a2359bd21e483a78b0e9c4be3
sahirooh <sahirooh@gmail.com>
2024-02-03 19:09:12 +0530

    updated useEffect

--- components/shared/CategoryFilter.tsx
+++ components/shared/CategoryFilter.tsx
@@ -22,7 +22,6 @@ const CategoryFilter = () => {
     const getCategories = async () => {
       const categoryList = await getAllCategories();
 
-      categoryList && setCategories(categoryList as ICategory[]);
     };
 
     getCategories();
