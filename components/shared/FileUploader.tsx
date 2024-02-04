cc639b40c6e496291c2b0e6d16c2e04cc43fb7e7
sahirooh <sahirooh@gmail.com>
2024-02-04 21:54:17 +0530

    updated fileUploader fn

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -18,7 +18,6 @@ export function FileUploader({
   imageUrl, onFieldChange, setFiles,
 }: FileUploaderProps) {
   const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
-    setFiles(acceptedFiles);
     onFieldChange(convertFileToUrl(acceptedFiles[0]));
   }, []);
 
