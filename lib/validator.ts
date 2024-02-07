b365c67ca5cd5e3ca0fb384180427a0d911c0114
sahirooh <sahirooh@gmail.com>
2024-02-07 22:05:29 +0530

    restructured validator

--- lib/validator.ts
+++ lib/validator.ts
@@ -7,8 +7,7 @@ export const eventFormSchema = z.object({
     .min(3, "Description must be at least 3 characters.")
     .max(400, "Description must be less than 400 characters."),
   location: z
-    .string()
-    .min(3, "Location must be at least 3 characters.")
+    .string().min(3, "Location must be at least 3 characters.")
     .max(400, "Location must be less than 400 characters."),
   imageUrl: z.string(),
   startDateTime: z.date(),
