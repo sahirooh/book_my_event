3b4461c7687e95223d8f91bb9ea79998c2c6426d
sahirooh <sahirooh@gmail.com>
2024-02-08 22:28:16 +0530

    updated utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -15,6 +15,7 @@ export const formatDateTime = (dateString: Date) => {
     month: "short",
     day: "numeric", // numeric day of the month (e.g., '25')
     hour: "numeric", // numeric hour (e.g., '8')
+    minute: "numeric", // numeric minute (e.g., '30')
     hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
   };
 
