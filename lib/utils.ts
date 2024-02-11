64b9c998b5214f19b255209003045f4f61a3d1c5
sahirooh <sahirooh@gmail.com>
2024-02-11 20:52:53 +0530

    updated utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -20,7 +20,7 @@ export const formatDateTime = (dateString: Date) => {
   };
 
   const dateOptions: Intl.DateTimeFormatOptions = {
-    weekday: "short", // abbreviated weekday name (e.g., 'Mon')
+    weekday: "short", 
     month: "short", // abbreviated month name (e.g., 'Oct')
     year: "numeric", // numeric year (e.g., '2023')
     day: "numeric", // numeric day of the month (e.g., '25')
