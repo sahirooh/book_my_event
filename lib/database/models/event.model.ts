09a0a44891abbb6aee6c6eba89756e111e8b768d
sahirooh <sahirooh@gmail.com>
2024-02-13 22:03:20 +0530

    updated interface

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -2,7 +2,6 @@ import { Document, Schema, model, models } from "mongoose";
 
 export interface IEvent extends Document {
     _id: string;
-    title: string;
     description?: string;
     location?: string;
     createdAt: Date;
