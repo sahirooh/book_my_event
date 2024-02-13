c207a5f27a3213025f78b8a5e1230918daff1fa4
sahirooh <sahirooh@gmail.com>
2024-02-13 22:04:41 +0530

    updated interface

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -4,7 +4,6 @@ export interface IEvent extends Document {
     _id: string;
     title: string;
     description?: string;
-    location?: string;
     createdAt: Date;
     imageUrl: string;
     startDateTime: Date;
