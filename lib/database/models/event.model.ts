5c15ec3d7a347204e33c4037b4de8ddef1376521
sahirooh <sahirooh@gmail.com>
2024-02-14 21:59:41 +0530

    updated eventSchema

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -19,6 +19,7 @@ export interface IEvent extends Document {
 const EventSchema = new Schema({
     title: {type: String, required: true},
     description: {type: String},
+    location: {type: String},
     createdAt: {type: Date, default: Date.now},
     imageUrl: {type: String, required: true},
     startDateTime: {type: Date, default: Date.now},
