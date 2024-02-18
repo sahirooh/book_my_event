00232d70621b86fb7b890f4359ef06876f6e2d40
sahirooh <sahirooh@gmail.com>
2024-02-18 21:36:13 +0530

    updated newEvent

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -40,6 +40,7 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
     if (!organizer) throw new Error("Organizer not found");
 
     const newEvent = await Event.create({
+      ...event,
       category: event.categoryId,
       organizer: userId,
     });
