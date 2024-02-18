89e8799f1439c18292d7e0463a0a2970a318c247
sahirooh <sahirooh@gmail.com>
2024-02-18 21:37:07 +0530

    updated newEvent

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -42,7 +42,6 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
     const newEvent = await Event.create({
       ...event,
       category: event.categoryId,
-      organizer: userId,
     });
     revalidatePath(path);
 
