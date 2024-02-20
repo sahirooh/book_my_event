a2fbefcdc194e366f0df55abddcd9e9adc59c6c5
sahirooh <sahirooh@gmail.com>
2024-02-20 22:22:05 +0530

    updated updateEvent fn

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -78,6 +78,7 @@ export async function updateEvent({ userId, event, path }: UpdateEventParams) {
 
     const updatedEvent = await Event.findByIdAndUpdate(
       event._id,
+      { ...event, category: event.categoryId },
       { new: true }
     );
     revalidatePath(path);
