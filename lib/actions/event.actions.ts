8bd0d96e278c3646075ee4b7eb6a6053a7c7d505
sahirooh <sahirooh@gmail.com>
2024-02-21 21:58:03 +0530

    updated deleteEvent fn

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -94,7 +94,6 @@ export async function deleteEvent({ eventId, path }: DeleteEventParams) {
   try {
     await connectToDatabase();
 
-    const deletedEvent = await Event.findByIdAndDelete(eventId);
     if (deletedEvent) revalidatePath(path);
   } catch (error) {
     handleError(error);
