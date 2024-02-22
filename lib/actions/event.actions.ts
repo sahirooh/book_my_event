ad72c6ba728a887eb33b878fb8b7ca3235008cc9
sahirooh <sahirooh@gmail.com>
2024-02-22 21:31:41 +0530

    updated getAllEvents fn

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -103,7 +103,6 @@ export async function deleteEvent({ eventId, path }: DeleteEventParams) {
 
 export async function getAllEvents({
   query,
-  limit = 6,
   page,
   category,
 }: GetAllEventsParams) {
