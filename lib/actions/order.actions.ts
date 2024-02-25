f44d54b083777665e4cb2ff92e8e160d63aae3c2
sahirooh <sahirooh@gmail.com>
2024-02-25 21:39:51 +0530

    updated createOrder

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -59,6 +59,7 @@ export const createOrder = async (order: CreateOrderParams) => {
       buyer: order.buyerId,
     });
 
+    return JSON.parse(JSON.stringify(newOrder));
   } catch (error) {
     handleError(error);
   }
