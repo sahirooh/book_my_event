c3ce54e61ebff70de40a39ec4b0981e3fc4994c9
sahirooh <sahirooh@gmail.com>
2024-02-28 22:39:33 +0530

    updated getOrdersByUser fn

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -139,6 +139,7 @@ export async function getOrdersByUser({
     const conditions = { buyer: userId };
 
     const orders = await Order.distinct("event._id")
+      .find(conditions)
       .sort({ createdAt: "desc" })
       .skip(skipAmount)
       .limit(limit)
