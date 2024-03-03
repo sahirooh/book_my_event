def7b3f96ac6116f0a6035f5cd6bfb73ea694761
sahirooh <sahirooh@gmail.com>
2024-03-03 22:50:24 +0530

    updated catFilter

--- components/shared/CategoryFilter.tsx
+++ components/shared/CategoryFilter.tsx
@@ -43,6 +43,7 @@ const CategoryFilter = () => {
   return (
     <Select onValueChange={(value: string) => onSelectCategory(value)}>
       <SelectTrigger className="select-field">
+        <SelectValue placeholder="Category" />
       </SelectTrigger>
       <SelectContent>
         <SelectItem value="All" className="select-item p-regular-14">
