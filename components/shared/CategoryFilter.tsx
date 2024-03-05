aa189a27b1d6c2ac396983bf49077c4fbee22fc7
sahirooh <sahirooh@gmail.com>
2024-03-05 23:41:57 +0530

    updated onSelectCategory

--- components/shared/CategoryFilter.tsx
+++ components/shared/CategoryFilter.tsx
@@ -26,6 +26,7 @@ const CategoryFilter = () => {
 
     if (category && category !== "All") {
       newUrl = formUrlQuery({
+        params: searchParams.toString(),
         key: "query",
         value: category,
       });
