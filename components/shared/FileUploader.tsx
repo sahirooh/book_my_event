baae75883f1695062f8d3938982b5b75c4bf93d4
sahirooh <sahirooh@gmail.com>
2024-03-13 10:16:51 +0530

    updated imageUrl

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -38,7 +38,6 @@ export function FileUploader({ // eslint-disable-line @typescript-eslint/explici
       {imageUrl ? (
         <div className="flex h-full w-full flex-1 justify-center ">
           <img
-            src={imageUrl}
             alt="image"
             width={250}
             height={250}
