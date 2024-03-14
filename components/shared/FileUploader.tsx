475a6aa1cd384b61a4fbcf10d2c19523fd087390
sahirooh <sahirooh@gmail.com>
2024-03-14 10:09:00 +0530

    updated fileUploader

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -17,7 +17,6 @@ export function FileUploader({ // eslint-disable-line @typescript-eslint/explici
   imageUrl, 
   onFieldChange,
   setFiles,
-}: FileUploaderProps) {
   const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
     setFiles(acceptedFiles);
     onFieldChange(convertFileToUrl(acceptedFiles[0]));
