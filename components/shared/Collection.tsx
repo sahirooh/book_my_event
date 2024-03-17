6eafda1c52fbd2fbb1a081e47ea2240a2f20a39f
Sahil Tembhurkar <101425007+sahirooh@users.noreply.github.com>
2024-03-17 20:54:50 +0530

    Update Collection.tsx

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -13,7 +13,7 @@ type CollectionProps = {
   urlParamName?: string;
   collectionType?: "Events_Organized" | "My_Tickets" | "All_Events";
 };
-
+// up to date
 const Collection = ({
   data,
   emptyTitle,
