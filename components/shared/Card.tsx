c9b1f8b5ee0a65bb2faca464c576cd7140fadeab
Sahil Tembhurkar <101425007+sahirooh@users.noreply.github.com>
2024-04-11 14:49:18 +0530

    Update Card.tsx

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -15,7 +15,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
-  
+  const isEventCreator = userId === event.organizer._id.toString();
 
   return (
     <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[440px]">
