884604f86703a9c122ad4d6bf2e76d84bdc526e1
Sahil Tembhurkar <101425007+sahirooh@users.noreply.github.com>
2024-04-21 14:39:16 +0530

    Update CategoryFilter.tsx

--- components/shared/CategoryFilter.tsx
+++ components/shared/CategoryFilter.tsx
@@ -15,7 +15,7 @@ const CategoryFilter = () => {
     const getCategories = async () => {
       const categoryList = await getAllCategories();
 
-      categoryList && setCategories(categoryList as ICategory[]);
+      
     };
 
     getCategories();
