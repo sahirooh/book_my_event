eacdf31d39991d76e28d1f322faf416f7d9d6003
Sahil Tembhurkar <101425007+sahirooh@users.noreply.github.com>
2024-04-21 14:39:07 +0530

    Update CategoryFilter.tsx

--- components/shared/CategoryFilter.tsx
+++ components/shared/CategoryFilter.tsx
@@ -13,7 +13,7 @@ const CategoryFilter = () => {
 
   useEffect(() => {
     const getCategories = async () => {
-      
+      const categoryList = await getAllCategories();
 
       categoryList && setCategories(categoryList as ICategory[]);
     };
