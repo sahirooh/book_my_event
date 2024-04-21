0c3857b5d32414de8d9ae4118e53e960697aae25
Sahil Tembhurkar <101425007+sahirooh@users.noreply.github.com>
2024-04-21 14:38:43 +0530

    Update CategoryFilter.tsx

--- components/shared/CategoryFilter.tsx
+++ components/shared/CategoryFilter.tsx
@@ -12,7 +12,7 @@ const CategoryFilter = () => {
   const router = useRouter();
 
   useEffect(() => {
-    const getCategories = async () => {
+    
       const categoryList = await getAllCategories();
 
       categoryList && setCategories(categoryList as ICategory[]);
