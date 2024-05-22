5439a95351ba73f33aadd3c6c9b7892493f7dc34
Sahil Tembhurkar <101425007+sahirooh@users.noreply.github.com>
2024-05-22 20:38:47 +0530

    Update select.tsx

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -25,7 +25,7 @@ const SelectTrigger = React.forwardRef<
     {...props}
   >
     {children}
-    
+    <SelectPrimitive.Icon asChild>
       <ChevronDown className="h-4 w-4 opacity-50" />
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
