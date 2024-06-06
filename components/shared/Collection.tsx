d417fff0648ba483093f0b108cd218a79311a9a0
Sahil Tembhurkar <101425007+sahirooh@users.noreply.github.com>
2024-06-06 21:21:58 +0530

    Update Collection.tsx

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -2,7 +2,7 @@ import { IEvent } from "@/lib/database/models/event.model";
 import React from "react";
 import Card from "./Card";
 import Pagination from "./Pagination";
-//collectionProps needs to be checked
+
 type CollectionProps = {
   data: IEvent[];
   emptyTitle: string;
