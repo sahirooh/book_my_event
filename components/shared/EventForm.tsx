0a862468fb376026eb98c2b8d02655d381976c16
Sahil Tembhurkar <101425007+sahirooh@users.noreply.github.com>
2024-06-13 20:32:16 +0530

    Update EventForm.tsx

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -32,7 +32,7 @@ import { IEvent } from "@/lib/database/models/event.model";
 
 type EventFormProps = {
   userId: string;
-  type: "Create" | "Update";
+  
   event?: IEvent;
   eventId?: string;
 };
