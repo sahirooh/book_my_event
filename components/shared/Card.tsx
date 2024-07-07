8e476209a0b3ef3b0adbed433ca272c209808f85
Sahil Tembhurkar <101425007+sahirooh@users.noreply.github.com>
2024-07-07 21:10:03 +0530

    Update Card.tsx

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -40,7 +40,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
       <div className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4">
         {!hidePrice && (
           <div className="flex gap-2">
-            
+            <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
               {event.isFree ? "FREE" : `$${event.price}`}
             </span>
             <p className="p-semibold-14 w-min rounded-full line-clamp-1 bg-grey-500/10 px-4 py-1 text-grey-500">
