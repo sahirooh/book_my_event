aab475e6d3f9e55734228d35607da19714f11d41
Sahil Tembhurkar <101425007+sahirooh@users.noreply.github.com>
2024-07-08 22:54:31 +0530

    Update CheckoutButton.tsx

--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -24,7 +24,7 @@ const CheckoutButton = ({ event }: { event: IEvent }) => {
             </Button>
           </SignedOut>
 
-          <SignedIn>
+          
             <Checkout event={event} userId={userId} />
           </SignedIn>
         </>
