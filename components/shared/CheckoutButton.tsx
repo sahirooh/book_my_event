a766ed2686de2eca41f488b19e6e449d2ac01c77
Sahil Tembhurkar <101425007+sahirooh@users.noreply.github.com>
2024-08-09 23:08:06 +0530

    Update CheckoutButton.tsx

--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -20,7 +20,7 @@ const CheckoutButton = ({ event }: { event: IEvent }) => {
         <>
           <SignedOut>
             <Button asChild className="button rounded-full" size="lg">
-              
+              <Link href="/sign-in">Get Tickets</Link>
             </Button>
           </SignedOut>
 
