f607395024d64a792ec893fc32b267ee4266293d
Sahil Tembhurkar <101425007+sahirooh@users.noreply.github.com>
2025-10-19 22:06:00 +0530

    Update Card.tsx

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -12,7 +12,7 @@ type CardProps = {
 };
 
 const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
-  
+  const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
   const isEventCreator = userId === event.organizer._id.toString();
